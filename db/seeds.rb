9cc25afe5b9b7c34b723378c8ea309039bd84234
Freddy Abelard <abelard.freddy@gmail.com>
2022-02-16 15:18:28 -0500

    Refactor the code and it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "Restaurant #{restaurant.id} was created"
+end
+
+puts "All good"
